fbd814a6bfcea2d3e4f929c72ba67ef9d5af76eb
Bulat F <write2bulat@gmail.com>
2017-07-21 16:17:38 +0300

    Install Bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <a className="btn btn-info">
+          Click Here
+        </a>
       </div>
     );
   }
