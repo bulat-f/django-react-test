cdb4f945dccff41ba43c171a2678f453d43da280
Bulat F <write2bulat@gmail.com>
2017-08-01 12:18:57 +0300

    Correct user registration (backend)

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,7 +1,27 @@
+import jwt
 from rest_framework import serializers
+from rest_framework_jwt.utils import jwt_payload_handler
 from django.contrib.auth.models import User
+from legrande_app.settings import SECRET_KEY
 
 class UserSerializer(serializers.ModelSerializer):
+    token = serializers.SerializerMethodField()
+
     class Meta:
         model = User
-        fields = ('id', 'username', 'first_name', 'last_name', 'email')
+        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'password', 'token')
+
+    def create(self, validated_data):
+        user = User(
+            username=validated_data['username'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    def get_token(self, user):
+        payload = jwt_payload_handler(user)
+        token = jwt.encode(payload, SECRET_KEY)
+        return token.decode('unicode_escape')
